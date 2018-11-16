5b8848b163c7588d45c6fc775654794526775f02
unknown <damian_morales_utn@hotmail.com>
2018-11-16 01:26:46 -0300

    fixed

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 app = express();
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
   
   app.get('/', (req, res) => res.render('pages/index'));
 
